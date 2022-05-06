ff25be3504a2855b450d36a6ecf97f24b73e9b64
LanaLebedeva <batetnikova@mail.ru>
2022-05-06 10:59:41 +0300

    add old style

--- frontend/components/pong.tsx
+++ frontend/components/pong.tsx
@@ -1,75 +1,123 @@
 import {FC, useEffect, useRef, useState} from 'react';
-import { io, Socket } from "socket.io-client";
+import {io, Socket} from "socket.io-client";
 
 import useEventListener from '../hooks/use_event_listener';
-import { FrameInfo, GameInfo, User } from '../types/interfaces';
+import {FrameInfo, GameInfo, User} from '../types/interfaces';
+
 
 interface PongProps {
-  gameInfo: GameInfo;
-  user: User | undefined;
-  userSessionId: string | undefined;
+    gameInfo: GameInfo;
+    user: User | undefined;
+    userSessionId: string | undefined;
 }
 
-const Pong: FC<PongProps> = ({ gameInfo, user, userSessionId }) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const socket = useRef<Socket>();
+const Pong: FC<PongProps> = ({gameInfo, user, userSessionId}) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const socket = useRef<Socket>();
 
-  const renderField = ({ ballX, ballY, club1Pos, club2Pos }: FrameInfo) => {
-    const canvas = canvasRef.current;
-    const ctx = canvasRef.current?.getContext('2d');
-    if (!canvas || !ctx) return;
+    const renderField = ({ballX, ballY, club1Pos, club2Pos}: FrameInfo) => {
+        const canvas = canvasRef.current;
+        const ctx = canvasRef.current?.getContext('2d');
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // счет от сервера
+        const left = 1230;
+        const right = 4567;
 
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        if (!canvas || !ctx) {
+            console.log("Update your browser, canvas is not supported in the current version")
+            return;
+        }
 
-    ctx.fillStyle = 'rgb(255, 255, 255)';
+        const ballSize = canvas.width / 24 //10;
+        const paddleHeight = ballSize * 5;
+        const paddleTab = ballSize;
+        // const maxPaddleY = canvas.height - ballSize - paddleHeight;
 
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, 10, 0, 2 * Math.PI);
-    ctx.fill();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillRect(20, club1Pos - 80, 20, 160);
-    ctx.fillRect(canvas.width - 40, club2Pos - 80, 20, 160);
-  };
+        //ball
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const toggleGameRunning = async () => {
-    const response = await fetch(`/api/games/${gameInfo.gameId}/toggle`, { method: 'POST' });
-  };
+        //score
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
+        document.getElementsByTagName('head')[0].appendChild(link);
+        ctx.font = '50px "Press Start 2P"'
+        ctx.textBaseline = 'top';
+        ctx.fillStyle = "darkgrey";
+        ctx.textAlign = "center";
+        ctx.fillText(left + '   ' + right, canvas.width / 2, (3 * ballSize));
 
-  useEffect(() => {
-    if (socket.current)
-      return;
+        //centerline
+        ctx.fillStyle = 'lightgrey';
+        for (let i = ballSize; i < canvas.height - ballSize; i += ballSize * 2) {
+            ctx.fillRect(canvas.width / 2 - ballSize / 2, i, ballSize / 4, ballSize / 2);
+        }
 
-    socket.current = io('http://localhost:4000');
-    socket.current?.emit('connectToGame', gameInfo.gameId);
+        //left
+        ctx.fillStyle = 'white';
+        ctx.fillRect(ballX, ballY, ballSize, ballSize);
 
-    socket.current?.on('nextFrame', (frame: FrameInfo) => {
-      renderField(frame);
-    });
-  }, []);
 
-  const keyHandler = (e: KeyboardEvent) => {
-    let delta = 0;
-    if (e.code == 'ArrowUp') delta = -20;
-    else if (e.code == 'ArrowDown') delta = 20;
+        // if (club1Pos < ballSize) {
+        //     ctx.fillRect(paddleTab, ballSize, ballSize, paddleHeight); // 160
+        // } else if (club1Pos > maxPaddleY) {
+        //     ctx.fillRect(paddleTab, maxPaddleY, ballSize, paddleHeight);
+        // } else {
+        //     ctx.fillRect(paddleTab, club1Pos, ballSize, paddleHeight); // 160
+        // }
+        // ;
 
-    if (delta != 0 &&
-        (gameInfo.player1?.id == user?.id ||
-         gameInfo.player2?.id == user?.id)) {
-      socket.current?.emit('moveClub', { userSessionId, delta });
-    }
-  };
+        ctx.fillRect(paddleTab, club1Pos, ballSize, paddleHeight);
 
-  useEventListener('keydown', keyHandler, document);
+        ctx.fillRect(canvas.width - paddleTab - ballSize, club2Pos, ballSize, paddleHeight); // 160
+    };
 
-  return (
-    <>
-      <canvas width={800} height={600} ref={canvasRef}></canvas>
-      <button onClick={toggleGameRunning}>Run / Pause</button>
-    </>
-  );
+    const toggleGameRunning = async () => {
+        const response = await fetch(`/api/games/${gameInfo.gameId}/toggle`, {method: 'POST'});
+    };
+
+    useEffect(() => {
+        if (socket.current)
+            return;
+
+        socket.current = io('http://localhost:4000');
+        socket.current?.emit('connectToGame', gameInfo.gameId);
+
+        socket.current?.on('nextFrame', (frame: FrameInfo) => {
+            renderField(frame);
+        });
+    }, []);
+
+    const keyHandler = (e: KeyboardEvent) => {
+        let delta = 0;
+        if (e.code == 'ArrowUp') delta = -20;
+        else if (e.code == 'ArrowDown') delta = 20;
+
+        if (delta != 0 &&
+            (gameInfo.player1?.id == user?.id ||
+                gameInfo.player2?.id == user?.id)) {
+            socket.current?.emit('moveClub', {userSessionId, delta});
+        }
+    };
+
+    useEventListener('keydown', keyHandler, document);
+
+    return (
+        <>
+            <head>
+                <meta charSet={"UTF-8"}/>
+                <title>Ping-pong</title>
+            </head>
+            <body>
+            <canvas width={800} height={600} ref={canvasRef}></canvas>
+            <button onClick={toggleGameRunning}>Run / Pause</button>
+            </body>
+        </>
+    );
 };
 
 export default Pong;
