9f31711075be560020fe5158ea1fcb51b1010bda
pharick <artemforkunov@gmail.com>
2022-05-22 13:21:40 +0300

    user 404

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { User } from './user.entity';
@@ -12,5 +18,12 @@ export class UsersController {
   @Get()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Get(':userId')
+  async findOne(@Param('userId') userId: number): Promise<User> {
+    const user: User = await this.usersService.findOne(userId);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 }
