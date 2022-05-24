2683fd88a7ac5947b0135ab0bb1ed41f5ef4b55c
pharick <artemforkunov@gmail.com>
2022-05-24 18:06:28 +0300

    brand

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 import UserBlock from '../components/userBlock';
 import { userContext } from './userProvider';
+import Link from 'next/link';
 
 const Header: FC = () => {
 
   return (
     <header className="header">
+      <Link href="/">
+        <a className="brand">ft_transcendence</a>
+      </Link>
+
       <userContext.Consumer>
         {({ user, handleLogout }) => (
           <UserBlock user={user} handleLogout={handleLogout} />
