1a32cefbf1b95bf723117151ccddb71d53947879
pharick <artemforkunov@gmail.com>
2022-09-19 10:30:19 +0300

    cute footer

--- frontend/components/layout/footer.tsx
+++ frontend/components/layout/footer.tsx
@@ -1,13 +1,31 @@
 import { FC } from 'react';
 
 import styles from '../../styles/Footer.module.css';
+import Link from 'next/link';
 
 const Footer: FC = () => {
   return (
     <footer className={`${styles.footer} mt-auto`}>
       <div className="row">
         <div className="col"></div>
-        <div className="col-auto">Made with love by team</div>
+        <div className="col-auto">
+          Made with 🤯 by{' '}
+          <Link href="https://profile.intra.42.fr/users/cbelva">
+            <a>cbelva</a>
+          </Link>
+          ,{' '}
+          <Link href="https://profile.intra.42.fr/users/jasougi">
+            <a>jasougi</a>
+          </Link>
+          ,{' '}
+          <Link href="https://profile.intra.42.fr/users/lsinistr">
+            <a>lsinistr</a>
+          </Link>{' '}
+          and{' '}
+          <Link href="https://profile.intra.42.fr/users/mlaureen">
+            <a>mlaureen</a>
+          </Link>
+        </div>
       </div>
     </footer>
   );
